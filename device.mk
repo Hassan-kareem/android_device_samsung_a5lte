42d59d0a7a44729c077436ec1c3aff017f06de86
Liu Wenyuan <15816141883@163.com>
2023-10-21 15:25:04 +0800

    a5lte: hidl: really just trash it
    hardware/lineage/compat already defines it
    
    Change-Id: I24b5c17799dcd3baf444c070a96f0ba7f40c79a8

--- device.mk
+++ device.mk
@@ -369,9 +369,7 @@ PRODUCT_PACKAGES += \
 # HIDL vendor support
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.base@1.0.vendor \
     android.hidl.manager@1.0 \
-    android.hidl.manager@1.0.vendor \
     libhidltransport \
     libhidltransport.vendor \
     libhwbinder \
