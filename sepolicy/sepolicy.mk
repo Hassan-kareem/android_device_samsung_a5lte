f9ac5ea8e34f38c1a270e78f8ca6275688b68757
Liu Wenyuan <15816141883@163.com>
2023-10-28 00:21:59 +0800

    a5lte: sepolicy: port sysfs_disk_stat stuff

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -26,4 +26,8 @@ BOARD_VENDOR_SEPOLICY_DIRS += \
     device/samsung/a5lte/sepolicy/common-lineage
 else
 $(warning building without lineageos sepolicies)
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
+    device/samsung/a5lte/sepolicy/public-aosp
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    device/samsung/a5lte/sepolicy/private-aosp
 endif
